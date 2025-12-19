3992f09ee3e6fa78739dd33d277e41b603828b4b
CoMoYo-Yanke <yanke1001@qq.com>
2025-12-19 09:38:34 +0800

    orginalView

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,19 +9,6 @@ import HomeView from './views/HomeView.vue';
 </script>
 
 <style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: Arial, sans-serif;
-}
-
-body {
-  font-family: 'Inter', -apple-system, BlinkMacSystemFont, sans-serif;
-  line-height: 1.6;
-  color: #2d3748;
-  background: #f7fafc;
-}
 
 #app {
   min-height: 100vh;
