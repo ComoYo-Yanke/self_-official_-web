f0220f31a57cd1d2c1f635f0e81ea42bead18696
CoMoYo-Yanke <yanke1001@qq.com>
2025-12-25 13:48:19 +0800

    all UserMapper

--- bankend/src/main/java/com/comoyo/controller/UserController.java
+++ bankend/src/main/java/com/comoyo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +22,6 @@ public class UserController {
         log.info("查询用户: {}", id);
         return Result.success(userService.getUserById(id));
     }
+    
+    
 }
