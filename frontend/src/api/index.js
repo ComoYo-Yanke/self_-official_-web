b8d538471100140e19c185902aec9bc5a8560318
CoMoYo-Yanke <yanke1001@qq.com>
2025-12-25 05:01:44 +0000

    删除文件 frontend

--- frontend/src/api/index.js
+++
@@ -1,9 +0,0 @@
-// 统一导出所有API模块
-export * from './auth.js'
-export * from './products.js'
-export * from './files.js'
-
-// 或者按需导入
-// import { authAPI } from './auth.js'
-// import { productAPI } from './products.js'
-// export { authAPI, productAPI }
--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-import './assets/style/components.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-
-createApp(App).mount('#app')
